726e48f5985b94593a59e99e5cdb4acf6c766049
RiyazAhamad <55573156+theriyazo@users.noreply.github.com>
2021-11-18 12:43:12 +0530

    fixed #1, by using let for its block scope

--- All Lessons/setTimeout.js
+++ All Lessons/setTimeout.js
@@ -2,7 +2,7 @@
 // for example 1 after 1sec, 2 after 2sec, 3 after 3sec and so on upto 5.
 
 function x(){
-    for (var i=0; i<5; i++){
+    for (let i=1; i<=5; i++){
         setTimeout(function (){
             console.log(i);
         }, i* 1000);
@@ -13,8 +13,8 @@ function x(){
 x();
 
 // Notes:
-// the following code doesnt work it only prints 5,5,5,5,5
-// Because till it waits for 1sec the value of i has become 5
-// and since each time the i is refered to the same memory the latest value is 5(because of closure).
+// now the code print 1 till 5, for each second
+// Because the let variables have block scope and the closure of calling function refers to unique value of i each and every itteration.
 
-// to avoid this we can use block scope variable which has unique value
+
+//⚡ In the next commit we'll see how the same is achived using var.
