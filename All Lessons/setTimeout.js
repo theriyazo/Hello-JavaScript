d0b0c9a57ab92eb4eead9dff204e9270407afe42
RiyazAhamad <55573156+theriyazo@users.noreply.github.com>
2021-11-18 13:41:42 +0530

    fixed #2, by using var with a closure function

--- All Lessons/setTimeout.js
+++ All Lessons/setTimeout.js
@@ -2,19 +2,20 @@
 // for example 1 after 1sec, 2 after 2sec, 3 after 3sec and so on upto 5.
 
 function x(){
-    for (let i=1; i<=5; i++){
-        setTimeout(function (){
-            console.log(i);
-        }, i* 1000);
+    for (var i=1; i<=5; i++){
+        function closure(i){
+            setTimeout(function (){
+                console.log(i);
+            }, i * 1000);
+        }
+        closure(i);   
     }
-    
     console.log("Hello JavaScript!");
 }
 x();
 
 // Notes:
 // now the code print 1 till 5, for each second
-// Because the let variables have block scope and the closure of calling function refers to unique value of i each and every itteration.
+// Because the function closure() creates a new copy each time by passing i into closure(i),
+// every time we call this closure() function with 'i' it creates new copy of the funtion and refers to that copy for every itteration
 
-
-//⚡ In the next commit we'll see how the same is achived using var.
