4794807f862b0375c667b1bab42c7af9cda27e2c
RiyazAhamad <55573156+theriyazo@users.noreply.github.com>
2021-11-27 16:29:50 +0530

    #5

---
+++ All Lessons/eventListener.js
@@ -0,0 +1,9 @@
+
+
+function formingClosure(){
+    let count = 0;
+    document.getElementById("clickMe").addEventListener("click", function xyz() {
+        console.log("Button Clicked : ", ++count);
+    });
+}
+formingClosure();
