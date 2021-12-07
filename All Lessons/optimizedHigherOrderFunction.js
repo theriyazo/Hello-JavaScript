81483f0a7f52dec47fbd34e0e52cef175cba2cce
RiyazAhamad <55573156+theriyazo@users.noreply.github.com>
2021-12-07 18:05:00 +0530

    optimization

--- All Lessons/optimizedHigherOrderFunction.js
+++ All Lessons/optimizedHigherOrderFunction.js
@@ -1,29 +1,29 @@
 
-// This way it obeys DRT- Don't Repeat Youself
-// And it is more readable and maintanable
+    // This way it obeys DRT- Don't Repeat Youself
+    // And it is more readable and maintanable
 
-const radius = [3, 1, 2, 4];
+    const radius = [3, 1, 2, 4];
 
-const area = function (radius){
-    return Math.PI * radius * radius;
-}
-
-const circumference = function (radius){
-    return 2 * Math.PI * radius;
-}
-
-const diameter = function (radius){
-    return 2 * radius;
-}
-
-const calculate = function (radius, logic){
-    const output = [];
-    for (let i=0; i<radius.length; i++){
-        output.push(logic(radius[i]));
+    const area = function (radius){
+        return Math.PI * radius * radius;
     }
-    return output; 
-}
 
-console.log(calculate(radius, area));
-console.log(calculate(radius, circumference));
-console.log(calculate(radius, diameter));
+    const circumference = function (radius){
+        return 2 * Math.PI * radius;
+    }
+
+    const diameter = function (radius){
+        return 2 * radius;
+    }
+
+    const calculate = function (radius, logic){
+        const output = [];
+        for (let i=0; i<radius.length; i++){
+            output.push(logic(radius[i]));
+        }
+        return output; 
+    }
+
+    console.log(calculate(radius, area));
+    console.log(calculate(radius, circumference));
+    console.log(calculate(radius, diameter));
